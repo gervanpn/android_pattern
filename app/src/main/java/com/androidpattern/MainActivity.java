bd0bf6556652ab71d7bb7000827b95077597487c
drThom <drthomsa@gmail.com>
2020-08-26 15:10:23 -0400

    activate Main button to ShoppingList

--- app/src/main/java/com/androidpattern/MainActivity.java
+++ app/src/main/java/com/androidpattern/MainActivity.java
@@ -2,11 +2,12 @@ package com.androidpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<intent> extends AppCompatActivity {
 Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +15,11 @@ Button loginBtn;
         setContentView(R.layout.activity_main);
         loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-            }
+                Intent intent = new Intent(MainActivity.this, ShoppingList.class);
+                startActivity(intent);}
         });
     }
 
