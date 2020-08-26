c91ffb17af8e8de097087de71aa40623bb7133d8
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-26 17:08:45 -0400

    adding buttons

--- app/src/main/java/com/androidpattern/ShoppingList.java
+++ app/src/main/java/com/androidpattern/ShoppingList.java
@@ -6,17 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ShoppingList extends AppCompatActivity {
 
-    Button checkoutBtn;
+    LinearLayout checkoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
-        checkoutBtn = (Button) findViewById(R.id.btn_checkout);
+        checkoutBtn = (LinearLayout) findViewById(R.id.btn_checkout);
         checkoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
