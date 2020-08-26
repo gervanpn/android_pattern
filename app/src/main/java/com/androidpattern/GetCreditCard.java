269d59cf85b5a2515d5baaed6c7ebc32abec298b
RoaaAlmadi <roaa.almadi@gmail.com>
2020-08-26 14:58:33 -0700

    the submit btn takes to the PaymentCart

--- app/src/main/java/com/androidpattern/GetCreditCard.java
+++ app/src/main/java/com/androidpattern/GetCreditCard.java
@@ -2,7 +2,9 @@ package com.androidpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.androidpattern.Models.CreditCardStrategy;
@@ -16,11 +18,11 @@ public class GetCreditCard extends AppCompatActivity {
         setContentView(R.layout.activity_get_credit_card);
 
         buttonSubmit = (Button) findViewById( com.androidpattern.R.id.buttonSubmit );
-        buttonSubmit.setOnClickListener(new android.view.View.OnClickListener() {
-
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(android.view.View v) {
-                android.widget.Toast.makeText( com.androidpattern.GetCreditCard.this , "Signing into PayPal" , android.widget.Toast.LENGTH_LONG ).show();
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PaymentCart.class);
+                startActivity(intent);
             }
         });
     }
