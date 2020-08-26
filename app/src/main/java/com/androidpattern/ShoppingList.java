bebb82f4e372bc2aea15543ed4f10ae39451c693
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-26 13:56:41 -0400

    second version of xml file

--- app/src/main/java/com/androidpattern/ShoppingList.java
+++ app/src/main/java/com/androidpattern/ShoppingList.java
@@ -2,7 +2,9 @@ package com.androidpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ShoppingList extends AppCompatActivity {
@@ -14,6 +16,14 @@ public class ShoppingList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
-        checkout
+        checkoutBtn = (Button) findViewById(R.id.btn_checkout);
+        checkoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PaymentCart.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
