12b66c96ec81700b7b020ac4467db71d3c90a7cf
aus <ausalmadi@gmail.com>
2020-08-26 14:50:56 -0400

    new changes

--- app/src/main/java/com/androidpattern/PaymentCart.java
+++ app/src/main/java/com/androidpattern/PaymentCart.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PaymentCart extends AppCompatActivity {
     Button creditCard, paypal;
+    TextView totalCost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_cart);
+        totalCost = findViewById(R.id.Total_textView);
 
         creditCard = (Button) findViewById(R.id.credit_card);
         paypal = (Button) findViewById(R.id.paypal);
