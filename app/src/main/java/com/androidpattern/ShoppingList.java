e583556c5ecbc0ccdbd8f98f480603638ae54576
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-26 13:31:33 -0400

    first version of xml file

--- app/src/main/java/com/androidpattern/ShoppingList.java
+++ app/src/main/java/com/androidpattern/ShoppingList.java
@@ -3,12 +3,17 @@ package com.androidpattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ShoppingList extends AppCompatActivity {
+
+    Button checkoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        checkout
     }
 }
