c28c0c96ee708ba79c153945b9a3eaa491b1c18f
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-27 15:18:05 -0400

    setting emtpy cart message to become invisible when cart in not empty

--- app/src/main/java/com/androidpattern/ShoppingList.java
+++ app/src/main/java/com/androidpattern/ShoppingList.java
@@ -7,15 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidpattern.Models.Cart;
 import com.androidpattern.Models.Item;
+
 import java.util.ArrayList;
 
 public class ShoppingList extends AppCompatActivity {
 
     ImageButton addBtn, checkoutBtn, removeItemBtn;
+    TextView emptyCartTV;
     EditText nameET, priceET, qntyET;
     Cart cart = new Cart();
 
@@ -24,12 +27,19 @@ public class ShoppingList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
+        //initializing Buttons
         addBtn = findViewById(R.id.btn_add);
         checkoutBtn = findViewById(R.id.btn_checkout);
         removeItemBtn = findViewById(R.id.btn_remove_item);
+        //initializing EditView
         nameET = findViewById(R.id.et_name);
         priceET = findViewById(R.id.et_price);
         qntyET = findViewById(R.id.et_qnty);
+        //initializing TextViews
+        emptyCartTV = findViewById(R.id.tv_empty_cart);
+
+        //check if cart is empty, show empty cart message
+        if (Cart.getQuantity() == 0) emptyCartTV.setVisibility(View.VISIBLE);
 
         //programing add button
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -47,16 +57,16 @@ public class ShoppingList extends AppCompatActivity {
                 } else if (Integer.parseInt(qnty) == 0) { //check if the quantity is not 0
                     Toast.makeText(getApplicationContext(), "Please enter a valid number of items", Toast.LENGTH_SHORT).show();
                     return;
-                } else {
-
-                    Item item = new Item(name, price, qnty); //creating an item based  the fields values
-                    cart.addItem(item); //adding new item to cart list
-                    Toast.makeText(getApplicationContext(), "Item added: " + item.getName(), Toast.LENGTH_SHORT).show();
-
-                    updateCartView();
-
-                    resetForm();  //clearing the form after adding the item successfully
                 }
+
+                //creating an item based  the fields values
+                Item item = new Item(name, price, qnty);
+                cart.addItem(item); //adding new item to cart list
+                Toast.makeText(getApplicationContext(), "Item added: " + item.getName(), Toast.LENGTH_SHORT).show();
+                if (Cart.getQuantity() != 0) emptyCartTV.setVisibility(View.INVISIBLE);
+                updateCartView();
+
+                resetForm();  //clearing the form after adding the item successfully
             }
 
         });
@@ -65,7 +75,7 @@ public class ShoppingList extends AppCompatActivity {
         checkoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (cart.getTotalCost() == 0) { // check if cart has any item to checkout; if not it returns;
+                if (Cart.getTotalCost() == 0) { // check if cart has any item to checkout; if not it returns;
                     Toast.makeText(getApplicationContext(), "Your shopping cart is empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
