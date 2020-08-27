8722a3eb69d8c4868298077dd0c54d23e0ea0bda
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-27 01:20:18 -0400

    updating buttons

--- app/src/main/java/com/androidpattern/ShoppingList.java
+++ app/src/main/java/com/androidpattern/ShoppingList.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class ShoppingList extends AppCompatActivity {
 
-    LinearLayout checkoutBtn;
+    ImageButton checkoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
-        checkoutBtn = (LinearLayout) findViewById(R.id.btn_checkout);
+        checkoutBtn =  findViewById(R.id.btn_checkout);
         checkoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
