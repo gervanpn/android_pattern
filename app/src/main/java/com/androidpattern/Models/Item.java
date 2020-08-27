1ab3156fc54f853d2349b5a308b505a01a1e74db
mojfazel <fazeli.mojtaba@gmail.com>
2020-08-27 09:31:17 -0400

    updating Item class

--- app/src/main/java/com/androidpattern/Models/Item.java
+++ app/src/main/java/com/androidpattern/Models/Item.java
@@ -1,4 +1,38 @@
 package com.androidpattern.Models;
 
 public class Item {
+
+    private String name;
+    private double price;
+    private int qnty;
+
+    public Item(String name, double price, int qnty) {
+        this.name = name;
+        this.price = price;
+        this.qnty = qnty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQnty() {
+        return qnty;
+    }
+
+    public void setQnty(int qnty) {
+        this.qnty = qnty;
+    }
 }
