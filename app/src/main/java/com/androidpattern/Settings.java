9ec6f5f62aab071b9a43af7564225b60023b3421
gervanpn <gervann@yahoo.ca>
2020-08-28 16:12:22 -0400

    Settings Updates

--- app/src/main/java/com/androidpattern/Settings.java
+++ app/src/main/java/com/androidpattern/Settings.java
@@ -2,13 +2,28 @@ package com.androidpattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    Button settings_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settings_save = findViewById(R.id.settings_save);
+
+        settings_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
